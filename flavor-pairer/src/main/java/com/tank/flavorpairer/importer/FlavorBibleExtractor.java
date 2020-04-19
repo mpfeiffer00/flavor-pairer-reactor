7f21afe0a6f4f91416f7073629ac4479894fb30a
tank <mp.7069@gmail.com>
2020-04-18 19:56:28 -0500

    Uses iText7 to parse the flavor bible in basic form

--- flavor-pairer/src/main/java/com/tank/flavorpairer/importer/FlavorBibleExtractor.java
+++ flavor-pairer/src/main/java/com/tank/flavorpairer/importer/FlavorBibleExtractor.java
@@ -3,29 +3,80 @@ package com.tank.flavorpairer.importer;
 import java.io.File;
 import java.io.IOException;
 
-import org.apache.pdfbox.cos.COSDocument;
-import org.apache.pdfbox.io.RandomAccessBufferedFileInputStream;
-import org.apache.pdfbox.pdfparser.PDFParser;
-import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.text.PDFTextStripper;
+import com.itextpdf.kernel.font.PdfFont;
+import com.itextpdf.kernel.geom.Rectangle;
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfReader;
+import com.itextpdf.kernel.pdf.canvas.parser.EventType;
+import com.itextpdf.kernel.pdf.canvas.parser.PdfCanvasProcessor;
+import com.itextpdf.kernel.pdf.canvas.parser.data.IEventData;
+import com.itextpdf.kernel.pdf.canvas.parser.data.TextRenderInfo;
+import com.itextpdf.kernel.pdf.canvas.parser.filter.TextRegionEventFilter;
+import com.itextpdf.kernel.pdf.canvas.parser.listener.FilteredEventListener;
+import com.itextpdf.kernel.pdf.canvas.parser.listener.LocationTextExtractionStrategy;
 
+// https://github.com/itext/itext7
+// https://itextpdf.com/en/resources/examples/itext-7/parsing-pdfs
 public class FlavorBibleExtractor {
+	public static final String DEST = "./target/txt/the_flavor_bible.txt";
+	public static final String SRC = "./target/txt/the_flavor_bible.pdf";
 
 	public static void main(String[] args) throws IOException {
-		PDFTextStripper pdfStripper = null;
-		PDDocument pdDoc = null;
-		final File file = new File("C:\\Users\\tank\\Documents\\Cook Books\\The Flavor Bible.pdf");
-		final PDFParser parser = new PDFParser(new RandomAccessBufferedFileInputStream(file));
-		parser.parse();
-		try (COSDocument cosDoc = parser.getDocument()) {
-			pdfStripper = new PDFTextStripper();
-			pdDoc = new PDDocument(cosDoc);
-			pdfStripper.setStartPage(76);
-			pdfStripper.setEndPage(76);
-			final String parsedText = pdfStripper.getText(pdDoc);
-			System.out.println(parsedText);
-		}
+		final File file = new File(DEST);
+		file.getParentFile().mkdirs();
 
+		new FlavorBibleExtractor().manipulatePdfOriginal(DEST);
 	}
 
+	public void manipulatePdfOriginal(String dest) throws IOException {
+		final PdfDocument pdfDoc = new PdfDocument(new PdfReader(SRC));
+
+		final Rectangle rect = new Rectangle(pdfDoc.getDefaultPageSize().getWidth(),
+				pdfDoc.getDefaultPageSize().getHeight());
+		final FilteredEventListener listener = new FilteredEventListener();
+
+		final LocationTextExtractionStrategy extractionStrategy = listener
+				.attachEventListener(new LocationTextExtractionStrategy(), new MovieFilter(rect));
+
+		// Ingredients: 76-1163
+		final PdfCanvasProcessor parser = new PdfCanvasProcessor(listener);
+		parser.processPageContent(pdfDoc.getPage(76));
+
+		final String actualText = extractionStrategy.getResultantText();
+
+		pdfDoc.close();
+
+		System.out.println(actualText);
+
+//		try (Writer writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(dest)))) {
+//			writer.write(actualText);
+//		}
+	}
+
+	private static final class MovieFilter extends TextRegionEventFilter {
+		public MovieFilter(Rectangle filterRect) {
+			super(filterRect);
+		}
+
+		@Override
+		public boolean accept(IEventData data, EventType type) {
+			if (!type.equals(EventType.RENDER_TEXT)) {
+				return false;
+			}
+
+			final TextRenderInfo renderInfo = (TextRenderInfo) data;
+			final PdfFont font = renderInfo.getFont();
+			if (font == null) {
+				return false;
+			}
+
+			final String fontName = font.getFontProgram().getFontNames().getFontName();
+			final boolean isBold = fontName.endsWith("Bold");
+			if (!isBold) {
+				return false;
+			}
+
+			return true;
+		}
+	}
 }
