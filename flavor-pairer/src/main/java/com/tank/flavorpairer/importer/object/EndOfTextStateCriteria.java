12d931cf35e594a8e1244024237bd37755a8504e
tank <mp.7069@gmail.com>
2020-06-30 20:27:18 -0500

    Added javadoc around state criteria

--- flavor-pairer/src/main/java/com/tank/flavorpairer/importer/object/EndOfTextStateCriteria.java
+++ flavor-pairer/src/main/java/com/tank/flavorpairer/importer/object/EndOfTextStateCriteria.java
@@ -7,6 +7,12 @@ import org.assertj.core.util.Lists;
 import com.tank.flavorpairer.importer.FlavorBibleIngredient;
 import com.tank.flavorpairer.importer.util.IngredientAttribute;
 
+/**
+ * Criteria of mutable fields about the processing line. I'm not proud of this.
+ * 
+ * @author tank
+ *
+ */
 public class EndOfTextStateCriteria {
 	public boolean hasUpdatedState;
 	public IngredientAttribute ingredientAttribute;
